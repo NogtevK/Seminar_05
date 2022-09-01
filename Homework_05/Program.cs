b1cf5e48d1b1c978e189f8f50508b65e0761baa4
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-09-01 05:08:27 +0300

    task 38 complete/testOK/commit and push

--- Homework_05/Program.cs
+++ Homework_05/Program.cs
@@ -42,7 +42,7 @@ Console.WriteLine("Четных чисел: " + count);
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
 
-void Print(int[] arr)
+/* void Print(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -65,8 +65,48 @@ for (int i = 1; i < mass.Length; i+=2)
     sumPos+=mass[i];
 }
 Console.WriteLine("Сумма элементов равна: "+sumPos);
-Console.WriteLine();
+Console.WriteLine(); 
+*/
 /////////////////////////////////////////////////////////////////////////////
 /* Задача 38: Задайте массив вещественных чисел. Найдите
 разницу между максимальным и минимальным
 элементов массива. */
+
+void Print(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+double[] mass = new double[10];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-10, 100) + new Random().NextDouble();
+}
+Print(mass);
+
+Console.WriteLine();
+
+double max = mass[0];
+double min = mass[0];
+
+for (int i = 1; i < mass.Length; i++)
+{
+    if (max < mass[i])
+    {
+        max = mass[i];
+    }
+}
+for (int j = 0; j < mass.Length; j++)
+{
+    if (min > mass[j])
+    {
+        min = mass[j];
+    }
+}
+Console.WriteLine("Max: " + max);
+Console.WriteLine("Min: " + min);
+Console.WriteLine($"Разница чисел: ({max}) - ({min}) = {max - min}");
